480ea79f8c4502d477dcbf1c74f204ecd4a2e193
R3turn0927 <bjleh0927@gmail.com>
2017-09-09 13:06:55 +0900

    Server 클래스 재명명, run 메소드 추가

--- body.py
+++ body.py
@@ -1,16 +1,23 @@
 from flask import *
-from setting import SETTING
+from _setting import SETTING
+from _html import HTML
 
 _KEEP_RUNNING, _WEBHOOK_MODE = True, True
 
-class SERVER():
+class _SERVER():
     app = Flask(__name__)
 
     def __init__(self):
-        setting = SETTING()
+        self.setting = SETTING()
+        self.html = HTML()
 
         if not _KEEP_RUNNING:
             exit()
 
-        self.app.config['SECRET_KEY'] = setting.FLASK['SECRET_KEY']
+        self.app.config['SECRET_KEY'] = self.setting.FLASK['SECRET_KEY']
 
+    def run(self, host="0.0.0.0", port=80):
+        self.app.run(host=host, port=port)
+
+project = _SERVER()
+project.run()
