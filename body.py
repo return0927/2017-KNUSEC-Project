d9072035835bdbd1b4da61fb92d713c82d5577b5
R3turn0927 <bjleh0927@gmail.com>
2017-09-09 13:01:24 +0900

    SERVER 클래스 생성

--- body.py
+++ body.py
@@ -1,7 +1,16 @@
 from flask import *
 from setting import SETTING
 
-_KEEP_RUNNING, _WEBHOOK_MODE = True
+_KEEP_RUNNING, _WEBHOOK_MODE = True, True
 
-app = Flask(__name__)
+class SERVER():
+    app = Flask(__name__)
+
+    def __init__(self):
+        setting = SETTING()
+
+        if not _KEEP_RUNNING:
+            exit()
+
+        self.app.config['SECRET_KEY'] = setting.FLASK['SECRET_KEY']
 
